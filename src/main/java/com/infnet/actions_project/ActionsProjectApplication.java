13f2fbf918e69a93aec5e796a052c43d33030541
flavionevesfsn <flavioneves89@gmail.com>
2025-12-03 21:53:24 -0300

    Created poroject

---
+++ src/main/java/com/infnet/actions_project/ActionsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.infnet.actions_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infnet/actions_project/ActionsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infnet.actions_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
